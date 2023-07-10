8ab8d13ad26034f6fbc80ec1f695749de041a49a
San Kang <devrivmt@gmail.com>
2023-07-10 20:03:31 +0900

    Design: Change app icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,21 +19,21 @@ export default function RootLayout({
       <head>
         <title>{metadata.title}</title>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" sizes="57x57" href="/images/favicons/apple-icon-57x57.png"/>
-        <link rel="apple-touch-icon" sizes="60x60" href="/images/favicons/apple-icon-60x60.png"/>
-        <link rel="apple-touch-icon" sizes="72x72" href="/images/favicons/apple-icon-72x72.png"/>
-        <link rel="apple-touch-icon" sizes="76x76" href="/images/favicons/apple-icon-76x76.png"/>
-        <link rel="apple-touch-icon" sizes="114x114" href="/images/favicons/apple-icon-114x114.png"/>
-        <link rel="apple-touch-icon" sizes="120x120" href="/images/favicons/apple-icon-120x120.png"/>
-        <link rel="apple-touch-icon" sizes="144x144" href="/images/favicons/apple-icon-144x144.png"/>
-        <link rel="apple-touch-icon" sizes="152x152" href="/images/favicons/apple-icon-152x152.png"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-icon-180x180.png"/>
-        <link rel="icon" type="image/png" sizes="192x192"  href="/images/favicons/android-icon-192x192.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="96x96" href="/images/favicons/favicon-96x96.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png"/>
+        <link rel="apple-touch-icon" sizes="57x57" href="/images/favicons/icon-57.png"/>
+        <link rel="apple-touch-icon" sizes="60x60" href="/images/favicons/icon-60.png"/>
+        <link rel="apple-touch-icon" sizes="72x72" href="/images/favicons/icon-72.png"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="/images/favicons/icon-76.png"/>
+        <link rel="apple-touch-icon" sizes="114x114" href="/images/favicons/icon-114.png"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="/images/favicons/icon-120.png"/>
+        <link rel="apple-touch-icon" sizes="144x144" href="/images/favicons/icon-144.png"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="/images/favicons/icon-152.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/icon-180.png"/>
+        <link rel="icon" type="image/png" sizes="192x192"  href="/images/favicons/icon-192.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/icon-32.png"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="/images/favicons/icon-96.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/icon-16.png"/>
         <meta name="msapplication-TileColor" content="#000000"/>
-        <meta name="theme-color" content="#9a9996"/>
+        <meta name="theme-color" content="#e91e63"/>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
